1c326f1b519e9ded1843c64d9c7007edcc82fe4c
buckie <wjmartino@gmail.com>
2015-02-17 02:01:24 -0500

    more failed attempts
    Signed-off-by: buckie <wjmartino@gmail.com>

--- src/Tsds/Prim/Types.hs
+++ src/Tsds/Prim/Types.hs
@@ -1,26 +1,25 @@
 
 module Tsds.Prim.Types where
 
-import           BasePrelude hiding (toList)
+import BasePrelude hiding (toList)
 import           Data.Vector.Unboxed hiding (replicate)
 import           Tsds.Prim.Unbox ()
 
 import qualified Text.PrettyPrint.Boxes as B
 
-type Date = Int32
-type DTime = Int64
-
 data Column = Int32_Col (Vector (Maybe Int32))
             | Int64_Col (Vector (Maybe Int64))
             deriving (Show, Read, Eq, Generic)
 
-type Table = [(String, Column)]
+type ColName = String
+
+type Table = [(ColName, Column)]
 
 printCell :: Show a => Maybe a -> String
 printCell (Just c) = show c
 printCell Nothing = "<null>"
 
-printCol :: (String, Column) -> B.Box
+printCol :: (ColName, Column) -> B.Box
 printCol (name, vals) = B.punctuateV B.center1 (B.text $ replicate (2+ B.cols col) '-') (header : [col])
   where
     header = B.text name
@@ -29,7 +28,7 @@ printCol (name, vals) = B.punctuateV B.center1 (B.text $ replicate (2+ B.cols co
         lst (Int32_Col v) = fmap printCell $ toList v
         lst (Int64_Col v) = fmap printCell $ toList v
 
-printTable :: [(String, Column)] -> IO ()
+printTable :: Table -> IO ()
 printTable table = B.printBox $ B.hsep 3 B.center1 $ printCol `fmap` table
 
 sample_table :: Table
