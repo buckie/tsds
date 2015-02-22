a73ec5c9ff8fb6bec318afffdc338e28424f6a89
buckie <wjmartino@gmail.com>
2015-02-22 03:03:44 -0500

    trying to get the printer working again, and I broke the cast system...
    Signed-off-by: buckie <wjmartino@gmail.com>

--- src/Tsds/Prim/Types.hs
+++ src/Tsds/Prim/Types.hs
@@ -1,91 +1,78 @@
-
 module Tsds.Prim.Types where
 
 import           BasePrelude hiding (toList)
-import           GHC.TypeLits
 
-import           Data.Proxy
-import qualified Data.IntSet as S
-import           Data.Default
+import           Data.Proxy ()
 
 import qualified Text.PrettyPrint.Boxes as B
 
-import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Generic as VG
-import qualified Data.Vector.Storable as VS
 
 import           Tsds.Prim.Unbox ()
 
-data Rec :: (u -> *) -> [u] -> * where
-  RNil :: Rec f '[]
-  (:&) :: !(f r) -> !(Rec f rs) -> Rec f (r ': rs)
+-- import           GHC.TypeLits
+-- import qualified Data.Vector as V
+-- import qualified Data.Vector.Storable as VS
+-- data Rec :: (u -> *) -> [u] -> * where
+--   RNil :: Rec f '[]
+--   (:&) :: !(f r) -> !(Rec f rs) -> Rec f (r ': rs)
 
-infixr :&
+-- infixr :&
+
+-- foo :: Rec ColStore [ '("Your Mou",Box,String), '("Age",UBox,Int16)]
+-- foo =  (CBox Proxy undefined) :&  (CUnbox Proxy undefined) :& RNil
+-- data Store = UBox | Box | Store
+
+-- data ColStore :: (Symbol, Store, *) -> * where
+--   CStored :: (KnownSymbol nm, VG.Vector VS.Vector a) => prox nm -> VS.Vector a -> ColStore '(nm, Stored, a)
+--   CUnbox  :: (KnownSymbol nm, VG.Vector VU.Vector a) => prox nm -> VU.Vector a -> ColStore '(nm, UBox,   a)
+--   CBox    :: (KnownSymbol nm, VG.Vector V.Vector  a) => prox nm -> V.Vector  a -> ColStore '(nm, Box,    a)
 
 
 data ColStoreExist :: * where
-    CStore :: (VG.Vector v b, Typeable b,Show b)=> v b -> ColStoreExist
-
-data Store = UBox | Box | Stored
-
-data ColStore :: (Symbol,Store,*) -> * where
-    CStored ::(KnownSymbol nm, VG.Vector VS.Vector a) => prox nm ->  VS.Vector a -> ColStore  '(nm,Stored,a)
-    CUnbox :: (KnownSymbol nm, VG.Vector VU.Vector a) => prox nm -> VU.Vector a -> ColStore '(nm,UBox,a)
-    CBox :: (KnownSymbol nm, VG.Vector V.Vector a) => prox nm -> V.Vector a -> ColStore '(nm,Box,a)
+  CStore :: (VG.Vector v b, Typeable b, Show b) => v b -> ColStoreExist
 
 type Int32_Col = VU.Vector (Maybe Int32)
 type Int64_Col = VU.Vector (Maybe Int64)
+type Date_Col  = VU.Vector (Maybe Int32)
+type DTime_Col = VU.Vector (Maybe Int64)
+
+-- class (Show a) => Nullable a where
+--   nullShow :: Maybe a -> String
+
+-- instance Nullable (Maybe Int32) where
+--   nullShow (Just c) = show c
+--   nullShow Nothing = "<null>"
+
+-- instance Nullable (Maybe Int64) where
+--   nullShow (Just c) = show c
+--   nullShow Nothing = "<null>"
 
 
-filterRows ::  forall t . (Typeable t)=> (t -> Bool ) -> ColStoreExist -> Either String S.IntSet
-filterRows pred  (CStore v) |  0 == VG.length v = Left "empty Column"
-                            | Nothing  <- myPred $ v VG.! 0  = Left "Error, MisMatched types, just like python"
-                            | otherwise =  Right $
-                              VG.ifoldl' (\ set idx val ->
-                                          maybe (error "wtf")
-                                                (\b  -> if b then S.insert idx set else set)
-                                          (myPred  (v VG.! idx)) )
-                                        S.empty  v
+type ColName = String
+type Table = [(ColName, ColStoreExist)]
 
-              where
-                myPred :: (Typeable a) => a -> Maybe Bool
-                myPred x= fmap pred $ cast x
-
-foo :: Rec ColStore [ '("Your Mou",Box,String), '("Age",UBox,Int16)]
-foo =  (CBox Proxy undefined) :&  (CUnbox Proxy undefined) :& RNil
-
-sample_table :: [(String, ColStoreExist)]
-sample_table = [ ("foo", v)
-               , ("bar", w)
-               , ("baz", x)
-               , ("bing", y)
-               ]
+printCol :: Typeable a => (a -> String) -> (String, ColStoreExist) -> B.Box
+printCol printMask (name, (CStore vals)) = B.punctuateV B.center1 (B.text $ replicate (2+ B.cols col) '-') (header : [col])
   where
-    v = CStore (VU.fromList [Just 1, Just 2, Just 3, Nothing] :: Int32_Col)
-    w = CStore (VU.fromList [Just 1, Just 2, Nothing, Just 4] :: Int32_Col)
-    x = CStore (VU.fromList [Just 1, Just 2, Nothing, Just 4] :: Int32_Col)
-    y = CStore (VU.fromList [Nothing, Just 2, Just 3, Just 4] :: Int32_Col)
+    header = B.text name
+    col = B.vcat B.center1 $ B.text `fmap` vals'
+      where
+        lst = VG.toList vals
+        mask' x = printMask `fmap` (cast x)
+        show' m x = case m x of
+                       Just y -> y
+                       Nothing -> error $ show x
+        vals' = (show' mask') `fmap` lst
 
-sample_col :: (String, ColStoreExist)
-sample_col = ("foo", CStore (VU.fromList [Just 1, Just 2, Just 3, Nothing] :: Int32_Col))
+printTable :: Typeable a => (a -> String) -> [(String, ColStoreExist)] -> IO ()
+printTable printMask table = B.printBox $ B.hsep 3 B.center1 $ (printCol printMask) `fmap` table
 
--- type ColName = String
--- type Table = [(ColName, Column)]
+noMask :: Show a => a -> String
+noMask = show
 
--- printCell :: Show a => Maybe a -> String
--- printCell (Just c) = show c
--- printCell Nothing = "<null>"
-
--- printCol :: (ColName, Column) -> B.Box
--- printCol (name, (WrapCol vals)) = B.punctuateV B.center1 (B.text $ replicate (2+ B.cols col) '-') (header : [col])
---   where
---     header = B.text name
---     col = B.vcat B.center1 $ B.text `fmap` lst vals
---       where
---         lst v = fmap printCell $ VU.toList v
-
--- printTable :: Table -> IO ()
--- printTable table = B.printBox $ B.hsep 3 B.center1 $ printCol `fmap` table
-
+nullMask :: Show a => Maybe a -> String
+nullMask (Just x) = show x
+nullMask Nothing = "<null>"
 
--- src/Tsds/SQL.hs
+++ src/Tsds/SQL.hs
@@ -5,9 +5,43 @@ import           BasePrelude
 
 import           Tsds.Prim.Types
 import qualified Data.IntSet as S
-import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Generic as VG
 
-type ColsClause  = [String]
+sample_table :: [(String, ColStoreExist)]
+sample_table = [ ("foo", v)
+               , ("bar", w)
+               , ("baz", x)
+               , ("bing", y)
+               ]
+  where
+    v = CStore (VU.fromList [Just 1, Just 2, Just 3, Nothing] :: Int32_Col)
+    w = CStore (VU.fromList [Just 1, Just 2, Nothing, Just 4] :: Int32_Col)
+    x = CStore (VU.fromList [Just 1, Just 2, Nothing, Just 4] :: Int32_Col)
+    y = CStore (VU.fromList [Nothing, Just 2, Just 3, Just 4] :: Int32_Col)
+
+sample_col :: (String, ColStoreExist)
+sample_col = ("foo", CStore (VU.fromList [Just 1, Just 2, Just 3, Nothing] :: Int32_Col))
+
+
+whereIdx ::  forall t . (Typeable t) => (t -> Bool) -> ColStoreExist -> Either String S.IntSet
+whereIdx fn (CStore v) | VG.null v == True              = Left "empty Column"
+                       | Nothing <- fn' $ v VG.! 0  = Left "Error, MisMatched types, just like python"
+                       | otherwise =  Right $
+                              VG.ifoldl' (\ set idx val ->
+                                          case fn' val of
+                                               Nothing   -> error "wtf"
+                                               Just True    -> S.insert idx set
+                                               _         -> set
+                                         )
+                                         S.empty  v
+
+              where
+                fn' = whereWrap fn
+
+whereWrap :: (Typeable a1, Typeable a) => (a -> b) -> a1 -> Maybe b
+whereWrap fn x = fn `fmap` cast x
+
 
 -- select :: ColsClause -> Table -> Table
 -- select (n:ns) table =
